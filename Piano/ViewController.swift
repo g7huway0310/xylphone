3c66000146cc3842bc733bcf31bdd94b98dbd027
lee <g7huway0310@gmail.com>
2020-07-22 12:13:26 +0800

    final

--- Piano/ViewController.swift
+++ Piano/ViewController.swift
@@ -7,19 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var XlyphonePic: UIImageView!
-    
+    var player:AVAudioPlayer?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let oneDegree = CGFloat.pi / 180
-        XlyphonePic.transform = CGAffineTransform.identity.rotated(by: oneDegree * 90)
+        
     }
 
-
+    @IBAction func SoundButton(_ sender: UIButton) {
+       playSound(SoundNane: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+        
+        sender.alpha = 1.0
+        }
+    }
+    
+    func playSound(SoundNane:String) {
+        let url = Bundle.main.url(forResource: SoundNane, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+       player?.play()
+                
+    }
 }
 
