9f2074a322178964b4f5272e53189651f16c3d76
lee <g7huway0310@gmail.com>
2020-07-22 10:29:51 +0800

    Initial Commit

---
+++ Piano/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Piano
+//
+//  Created by guowei on 2020/7/22.
+//  Copyright © 2020 guowei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
